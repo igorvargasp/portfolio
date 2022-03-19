6a960a0fb3bc623f3d22f1f083dce53ba0da3589
igor <igorvargasp@hotmail.com>
2022-03-19 02:09:39 -0300

    About me section finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Header from "./components/Header/Header.tsx";
-
+import AboutMe from "./components/AboutMe/AboutMe.tsx"
 
 
 function App() {
   return (
     <div>
     <Header/>
+    <AboutMe/>
     </div>
   );
 }
---
+++ src/components/AboutMe/AboutMe.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "./aboutMe.css";
+import { AiOutlineUser } from "react-icons/ai";
+
+const AboutMe: React.FC = () => {
+  return (
+    <div className="about-me-div">
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          flexDirection: "row",
+          marginTop: "5px",
+        }}
+      >
+        <div className="perfil-div">
+          <img
+            alt="teste"
+            src={require("../assets/img/perfil.jpg")}
+            className="perfil-photo"
+          />
+        </div>
+
+        <div className="description-div">
+          <div className="title-div">
+            <AiOutlineUser size="2.5vw" style={{ color: "#831BA8" }} />
+            <h3>About Me</h3>
+          </div>
+          <p>
+            My name is igor, i’m software developer. I've started to learn how
+            to code at my university where i graduated on 2022, majoring in
+            internet systems. I've got a scholarship as a backend developer at
+            compasso with a collab between my university and compasso. After 4
+            months i've got hired as chatbot developer intern and then after 8
+            months i've finished my internship on 2021. On december of 2021 i
+            got hired full time as a cloud developer by IBM and it's where i
+            work currently.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutMe;
